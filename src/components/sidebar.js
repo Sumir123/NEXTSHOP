3089f6b216b3931ac0a33e850f416d03cb0b1586
Sumir <sumir.bhandari11@gmail.com>
2023-04-03 12:34:02 +0545

    removed unnecessary files

--- src/components/sidebar.js
+++
@@ -1,5 +0,0 @@
-const SideBar = () => {
-  return <></>;
-};
-
-export default SideBar;
--- src/store/slices/useMenuSlice.js
+++
@@ -1,6 +0,0 @@
-const createMenuSlice = (set) => ({
-  isSidebarOpen: true,
-  setIsSidebarOpen: (isSidebarOpen) => set({ isSidebarOpen: !isSidebarOpen }),
-});
-
-export default createMenuSlice;
